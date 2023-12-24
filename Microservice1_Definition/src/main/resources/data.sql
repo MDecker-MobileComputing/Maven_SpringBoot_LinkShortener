758704f3fc1041e4e83c2f0823f7a24f846f1952
MDecker-MobileComputer <mide_42b@yahoo.de>
2023-12-24 16:54:32 +0100

    data.sql: MERGE INTO gefixt

--- Microservice1_Definition/src/main/resources/data.sql
+++ Microservice1_Definition/src/main/resources/data.sql
@@ -11,9 +11,9 @@ INSERT INTO urls (id, url_original, url_kuerzel, beschreibung, passwort, ist_aut
 
 /* MERGE statt INSERT, damit keine Fehlermeldung bei mehrfachem Ausführen der Datei auftritt. */
 
-MERGE INTO urls (id, url_original, url_kuerzel, beschreibung, passwort)
-    KEY(id)
-    VALUES (1,
+MERGE INTO urls (url_original, url_kuerzel, beschreibung, passwort)
+    KEY(url_kuerzel) -- Anhand Werten in Spalte "url_kuerzel" wird entschieden, ob ein Datensatz neu angelegt oder aktualisiert wird.
+    VALUES (
         'https://cloud.google.com/blog/products/gcp/7-ways-we-harden-our-kvm-hypervisor-at-google-cloud-security-in-plaintext?hl=en',
         'a',
         'Artikel von Google, in dem erwähnt wird, dass die Google Cloud Platform (GCP) die Sicherheit des Hypervisors "KVM" verbessert hat.',
@@ -21,9 +21,9 @@ MERGE INTO urls (id, url_original, url_kuerzel, beschreibung, passwort)
     );
 
 
-MERGE INTO urls (id, url_original, url_kuerzel, beschreibung, passwort)
-    KEY(id)
-    VALUES (2,
+MERGE INTO urls (url_original, url_kuerzel, beschreibung, passwort)
+    KEY(url_kuerzel)
+    VALUES (
         'https://subscription.packtpub.com/book/cloud-and-networking/9781789137231/17/ch17lvl1sec92/overview-of-docker-hub-and-its-operations',
         'b',
         'Docker Hub: pull and push images',
