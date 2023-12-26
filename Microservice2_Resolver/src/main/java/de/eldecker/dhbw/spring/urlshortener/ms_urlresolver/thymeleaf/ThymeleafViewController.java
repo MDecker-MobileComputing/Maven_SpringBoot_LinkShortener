60d39a8d3f3ad3140e794a8f111aa3906ad57a1e
MDecker-MobileComputing <mide_42b@yahoo.de>
2023-12-26 13:10:40 +0100

    MS2: Template-Seite weiter ausgebaut

--- Microservice2_Resolver/src/main/java/de/eldecker/dhbw/spring/urlshortener/ms_urlresolver/thymeleaf/ThymeleafViewController.java
+++ Microservice2_Resolver/src/main/java/de/eldecker/dhbw/spring/urlshortener/ms_urlresolver/thymeleaf/ThymeleafViewController.java
@@ -11,12 +11,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 /**
  * Die zugehörigen Template-Dateien finden sich im Ordner {@code src/main/resources/templates}.
  * Der Name des anzuzeigenden Template wird von den Methoden dieser Klasse als String zurückgegeben.
+ * <br><br>
+ *
+ * Wenn in der Template-Datei ein Fehler ist, dann wird erst zur Laufzeit beim ersten Aufruf dieser
+ * Seite eine Fehlermeldung im Log angezeigt.
  */
 @Controller
 public class ThymeleafViewController {
 
     /**
-     * Instanzname aus Konfigurationsdatei {@code application-XXX.properties}
+     * Instanzname aus Konfigurationsdatei {@code application-XXX.properties};
+     * wird auf Ergebnis-Seiten angezeigt, damit man Load-Balancing nachvollziehen kann.
      */
     @Value("${de.eldecker.linkshortener.ms2.instanzname}")
     private String _instanzname;
@@ -25,19 +30,23 @@ public class ThymeleafViewController {
     /**
      * HTTP-Get-Methode für das Auflösen eines URL-Kürzels.
      *
-     * @param kuerzel Pfadvariable aus der URL, enthält das aufzulösende Kürzel
+     * @param kuerzel Pfadvariable aus der URL, enthält das aufzulösende URL-Kürzel
      *
      * @param model Model-Objekt, das an das Template übergeben wird
      *
-     * @return Name der aufzurufenden Template-Datei
+     * @return Name der aufzurufenden Template-Datei "ergebnis.html" aus
+     *         Verzeichnis {@code src/main/resources/templates}
      */
     @GetMapping("/k/{kuerzel}")
     public String kuerzelAufloesen(@PathVariable String kuerzel, Model model) {
 
-        model.addAttribute("instanzname", _instanzname);
-        model.addAttribute("gefunden", false);
+        final String kuerzelTrimmed = kuerzel.trim();
 
-        return "ergebnis"; // Template-Datei ergebnis.html
+        model.addAttribute("instanzname", _instanzname   );
+        model.addAttribute("kuerzel"    , kuerzelTrimmed );
+        model.addAttribute("gefunden"   , false          );
+
+        return "ergebnis";
     }
 
 }
