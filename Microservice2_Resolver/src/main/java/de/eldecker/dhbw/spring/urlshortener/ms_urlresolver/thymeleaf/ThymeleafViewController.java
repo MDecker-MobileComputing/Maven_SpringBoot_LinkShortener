501ef1315dae1dfee284e3aad0a3cb4caa11895b
MDecker-MobileComputing <mide_42b@yahoo.de>
2023-12-26 11:32:52 +0100

    MS2: Instanzname wird ueber Template angezeigt

--- Microservice2_Resolver/src/main/java/de/eldecker/dhbw/spring/urlshortener/ms_urlresolver/thymeleaf/ThymeleafViewController.java
+++ Microservice2_Resolver/src/main/java/de/eldecker/dhbw/spring/urlshortener/ms_urlresolver/thymeleaf/ThymeleafViewController.java
@@ -1,8 +1,11 @@
 package de.eldecker.dhbw.spring.urlshortener.ms_urlresolver.thymeleaf;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 
 /**
@@ -12,18 +15,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 @Controller
 public class ThymeleafViewController {
 
+    /**
+     * Instanzname aus Konfigurationsdatei {@code application-XXX.properties}
+     */
+    @Value("${de.eldecker.linkshortener.ms2.instanzname}")
+    private String _instanzname;
+
 
     /**
      * HTTP-Get-Methode für das Auflösen eines URL-Kürzels.
      *
      * @param model Model-Objekt, das an das Template übergeben wird
      *
+     * @param kuerzel Pfadvariable aus der URL, enthält das aufzulösende Kürzel
+     *
      * @return Name der aufzurufenden Template-Datei
      */
-    @GetMapping("/k")
-    public String kuerzelAufloesen(Model model) {
+    @RequestMapping("/k/{kuerzel}")
+    public String kuerzelAufloesen(Model model, @PathVariable String kuerzel) {
 
-        model.addAttribute("message", "Lorem Ipsum");
+        model.addAttribute("instanzname", _instanzname);
+        model.addAttribute("gefunden", false);
+
         return "aufgeloest"; // Template-Datei aufgeloest.html
     }
 
