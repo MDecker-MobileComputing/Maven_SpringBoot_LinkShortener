397b54be5a02710362604e4aacd3f2cfeefb5e85
MDecker-MobileComputing <mide_42b@yahoo.de>
2023-12-29 16:47:20 +0100

    MS1: Form und JavaScript verbessert

--- Microservice1_Definition/src/main/resources/public/script.js
+++ Microservice1_Definition/src/main/resources/public/script.js
@@ -1,5 +1,13 @@
 "use strict";
 
+/**
+ * Diese Funktion prüft, ob eine URL eingegeben wurde.
+ * Die Prüfung erfolgt, wenn das Formular abgeschickt wird.
+ * Wenn keine URL eingegeben wurde, dann wird eine Fehlermeldung angezeigt
+ * und das Formular wird nicht abgeschickt.
+ *
+ * @returns {Boolean} true, wenn eine URL eingegeben wurde, sonst false
+ */
 function formularPruefen() {
 
     const x = document.forms["dasFormular"]["urlLang"].value;
@@ -7,5 +15,8 @@ function formularPruefen() {
 
         alert("FEHLER: Keine Ziel-URL eingegeben");
         return false;
+
     }
+
+    return true;
 }
