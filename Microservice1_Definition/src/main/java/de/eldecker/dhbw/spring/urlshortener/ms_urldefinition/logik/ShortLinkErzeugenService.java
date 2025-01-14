c8e213760eccdb1ffd5f9c93ad0afc4f479aa24b
MDecker-MobileComputing <mide_42b@yahoo.de>
2025-01-14 13:20:48 +0100

    Update ShortLinkErzeugenService.java

--- Microservice1_Definition/src/main/java/de/eldecker/dhbw/spring/urlshortener/ms_urldefinition/logik/ShortLinkErzeugenService.java
+++ Microservice1_Definition/src/main/java/de/eldecker/dhbw/spring/urlshortener/ms_urldefinition/logik/ShortLinkErzeugenService.java
@@ -12,6 +12,7 @@ import de.eldecker.dhbw.spring.urlshortener.ms_urldefinition.model.ShortLinkExce
 import java.util.Date;
 
 import org.apache.commons.validator.routines.UrlValidator;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Isolation;
 import org.springframework.transaction.annotation.Transactional;
@@ -36,6 +37,7 @@ public class ShortLinkErzeugenService {
     /**
      * Konstruktor für Dependency Injection.
      */
+    @Autowired
     public ShortLinkErzeugenService( Datenbank db,
                                      UrlValidator urlValidator,
                                      KafkaSender kafkaSender ) {
@@ -49,38 +51,41 @@ public class ShortLinkErzeugenService {
      * Neue ShortLink-Definition auf DB anlegen.
      *
      * @param urlLang Vom Nutzer eingegebene (Lang-)URL
+     * 
      * @param beschreibung Optionale Beschreibung der URL
+     * 
      * @return Objekt mit Kürzel und Passwort für die angelegte URL-Definition
+     * 
      * @throws ShortLinkException Wenn die URL ungültig ist oder ein DB-Fehler aufgetreten ist.
      */
     @Transactional(isolation = Isolation.SERIALIZABLE)
-    public KuerzelUndPasswort shortLinkAnlegen(String urlLang,
-                                               String beschreibung) throws ShortLinkException {
+    public KuerzelUndPasswort shortLinkAnlegen( String urlLang,
+                                                String beschreibung ) throws ShortLinkException {
 
         final String urlLangTrimmed      = urlLang.trim();
         final String beschreibungTrimmed = beschreibung.trim();
 
-        if (urlLangTrimmed.isBlank()) {
+        if ( urlLangTrimmed.isBlank() ) {
 
             throw new ShortLinkException("Leere URL von Nutzer erhalten.", false);
         }
-        if (urlLangTrimmed.length() > 999) {
+        if ( urlLangTrimmed.length() > 999 ) {
 
-            throw new ShortLinkException("Zu lange URL von Nutzer erhalten: " + urlLangTrimmed, false);
+            throw new ShortLinkException( "Zu lange URL von Nutzer erhalten: " + urlLangTrimmed, false );
         }
-        if (_urlValidator.isValid(urlLangTrimmed) == false) {
+        if ( _urlValidator.isValid(urlLangTrimmed) == false ) {
 
-            throw new ShortLinkException("Ungültige URL von Nutzer erhalten: " + urlLangTrimmed, false);
+            throw new ShortLinkException( "Ungültige URL von Nutzer erhalten: " + urlLangTrimmed, false );
         }
 
         final int maxId = _datenbank.holeMaxId();
-        if (maxId == -1) {
+        if ( maxId == -1 ) {
 
             throw new ShortLinkException("Datenbankfehler beim Ermitteln der maximalen ID.", true);
         }
 
         final int    idNeu          = maxId + 1;
-        final String kuerzel        = zahlZuString(idNeu);
+        final String kuerzel        = zahlZuString( idNeu );
         final String passwort       = erzeugePasswort();
         final Date   datumZeitJetzt = new Date();
 
@@ -91,19 +96,20 @@ public class ShortLinkErzeugenService {
                                                          beschreibungTrimmed,
                                                          datumZeitJetzt,
                                                          datumZeitJetzt,
-                                                         true);
-        _kafkaSender.sendeUrlDefinition(kud);
+                                                         true 
+                                                       );
+        _kafkaSender.sendeUrlDefinition( kud );
 
         // URL-Definition in eigener Datenbank anlegen
         boolean warErfolgreich = _datenbank.neueKurzUrl( urlLangTrimmed, kuerzel, beschreibungTrimmed, passwort, datumZeitJetzt);
-        if (warErfolgreich) {
+        if ( warErfolgreich ) {
 
-            return new KuerzelUndPasswort(kuerzel, passwort);
+            return new KuerzelUndPasswort( kuerzel, passwort );
 
         } else {
 
-            throw new ShortLinkException("Datenbankfehler beim Anlegen von URL-Definition für folgende URL-Definition: " +
-                                         urlLangTrimmed, true);
+            throw new ShortLinkException( "Datenbankfehler beim Anlegen von URL-Definition für folgende URL-Definition: " +
+                                          urlLangTrimmed, true );
         }
     }
 
